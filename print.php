98c51c2f2761f22b99ece9951ea18533c141eb05
takunnma528 <sennmonnkakidori@gmail.com>
2022-07-27 22:26:39 +0900

    なんこれ なんこれ

---
+++ print.php
@@ -0,0 +1,17 @@
+<!DOCTYPE_html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>検索中、、、。</title>
+</head>
+<body>
+    <!-- <h1>テストページ</h1> -->
+    <?php
+    // print_r($_POST);
+    echo '<meta http-equiv="refresh" content="0;url=https://www.google.com/search?q=';
+    echo htmlspecialchars($_POST['name'], ENT_QUOTES);
+    echo '">';
+    echo '<br>';
+    ?>
+</body>
+</html>
