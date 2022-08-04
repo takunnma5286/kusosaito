e94d597560e7416e7eb961a5aad1604ab3ff8014
takunnma528 <sennmonnkakidori@gmail.com>
2022-08-04 21:31:50 +0900

    適当に作りました、許して下さい。

--- print.php
+++ print.php
@@ -7,10 +7,29 @@
 <body>
     <?php
     // print_r($_POST);
+    // Mysql
+        $dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', 'test', 'test');
+        echo '<p>';
+        echo '接続完了しました';
+        echo '</p>';
+
+        $name = $_POST['do'];
+        $sql = 'INSERT INTO kensaku (rireki) VALUES (?)';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(1, $name, PDO::PARAM_STR);
+        $stmt->execute();
+        echo '<p>';
+        echo '記録しました';
+        echo '</p>';
+
+        $dbh = null;
+
+    echo '<p>';
+    echo '正常に処理が完了しました、googleなページへ転送します';
     echo '<meta http-equiv="refresh" content="0;url=https://www.google.com/search?q=';
-    echo htmlspecialchars($_POST['name'], ENT_QUOTES);
+    echo htmlspecialchars($_POST['do'], ENT_QUOTES);
     echo '">';
-    echo '<br>';
+    echo '</p>';
     ?>
 </body>
 </html>
